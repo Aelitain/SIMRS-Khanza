5ac152af85f5f426a7a3bd69e818851c6ef2fb8f
mas-elkhanza <khanza_media@yahoo.com>
2026-09-11 08:21:58 +0700

    Ringkasan Beban Hutang Lain

--- src/rekammedis/RMDataResumePasienRanap.java
+++ src/rekammedis/RMDataResumePasienRanap.java
@@ -2339,7 +2339,6 @@ public final class RMDataResumePasienRanap extends javax.swing.JDialog {
                 public void windowDeactivated(WindowEvent e) {}
             });
             carikeluhan.setNoRawat(TNoRw.getText());
-            carikeluhan.tampil();
             carikeluhan.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
             carikeluhan.setLocationRelativeTo(internalFrame1);
             carikeluhan.setVisible(true);
@@ -2522,7 +2521,6 @@ public final class RMDataResumePasienRanap extends javax.swing.JDialog {
                 public void windowDeactivated(WindowEvent e) {}
             });
             caripemeriksaan.setNoRawat(TNoRw.getText());
-            caripemeriksaan.tampil();
             caripemeriksaan.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
             caripemeriksaan.setLocationRelativeTo(internalFrame1);
             caripemeriksaan.setVisible(true);
